1d5296a76f5b16712692749f962373070d70b1bd
Leonardo Araujo <leonardo.aa88@gmail.com>
2024-06-19 16:53:59 -0300

    alternative func for string reversal

--- basic-functions/strings.hs
+++ basic-functions/strings.hs
@@ -2,6 +2,10 @@ reverseString :: String -> String
 reverseString [] = []
 reverseString (x : xs) = reverseString xs ++ [x]
 
+reverseString2 :: String -> String
+reverseString2 str = foldr (\x xs -> xs ++ [x]) [] str
+
 main :: IO ()
 main = do
   print (reverseString "Leonardo")
+  print (reverseString2 "Leonardo")
