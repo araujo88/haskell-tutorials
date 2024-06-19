67c527ce26aa5143e9a41015efcf98e53697642a
Leonardo Araujo <leonardo.aa88@gmail.com>
2024-06-19 02:02:41 -0300

    more recursion examples

--- basic-functions/function.hs
+++ basic-functions/function.hs
@@ -32,6 +32,37 @@ listLength :: [a] -> Int
 listLength [] = 0
 listLength list = 1 + listLength (tail list)
 
+power :: Integer -> Integer -> Integer
+power x 0 = 1
+power x n
+  | even n = y * y
+  | otherwise = y * y * x
+  where
+    y = power x (div n 2)
+
+-- 2^6 = 2^3 * 2^3 (even)
+-- 2^7 = 2^3 * 2^3 * 2 (odd)
+
+isPrime :: Int -> Bool
+isPrime 0 = False
+isPrime 1 = False
+isPrime x = not (hasDivisor (x - 1))
+  where
+    hasDivisor :: Int -> Bool
+    hasDivisor 1 = False
+    hasDivisor n = mod x n == 0 || hasDivisor (n - 1)
+
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+-- fibonacci 2 = fibonacci (2-1) + fibonacci (2-2)
+-- fibonacci 2 = fibonacci (1) + fibonacci (0) = 0 + 1 = 1
+
+-- fibonacci 3 = fibonacci (3-1) + fibonacci (3-2)
+-- fibonacci 3 = fibonacci 2 + fibonacci 1 = 1 + 1 = 2
+
 main :: IO ()
 main = do
   print (square (square 2))
@@ -46,4 +77,8 @@ main = do
   print (absVal 1)
   print (absVal (-1))
   print (listLength [31, 234, 21, 23, 1])
+  print (power 2 6)
+  print (power 2 7)
+  print (map isPrime [1 .. 10])
+  print (map fibonacci [1 .. 10])
   print (factorial (-1)) -- will throw an error
